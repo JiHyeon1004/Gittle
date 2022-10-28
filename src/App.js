5b4af3796c0a8e69b66e000b46d181d720245298
이선호 <zpfh1638@naver.com>
2022-10-28 17:54:27 +0900

    Routing Ready

--- src/App.js
+++ src/App.js
@@ -63,6 +63,34 @@ function Create(props){
   )
 }
 
+function Update(props){
+  const [title,setTitle]=useState(props.title)
+  const [body,setBody]=useState(props.body)
+  return(
+    <article>
+      <h2>Update</h2>
+      <form onSubmit={event=>{
+        event.preventDefault()
+        const title =event.target.title.value
+        const body = event.target.body.value
+        props.onUpdate(title,body)
+      }}>
+        <p><input name="title" placeholder='title' value={title} onChange={event=>{
+
+          setTitle(event.target.value)
+          //onChange는 값을 바꿀때마다 호출한다!
+        }}></input></p>
+        <p><textarea name="body" placeholder='body' value={body} onChange={event=>{
+
+          setBody(event.target.value)
+          //onChange는 값을 바꿀때마다 호출한다!
+        }}></textarea></p> 
+        <p><input type="submit" value="Update"></input></p>
+      </form>
+    </article>
+  )
+}
+
 function App() {
 
   const [nextId,setNextId]=useState(4)
@@ -79,6 +107,7 @@ function App() {
 
   let content = null;
   let title,body=null;
+  let contextControl=null;
   if(mode ==='WELCOME'){
     content=<Article title="WELCOME" body="Hello, WEB"></Article>
   }else if(mode ==='READ'){
@@ -92,6 +121,21 @@ function App() {
 
 
     content=<Article title={title} body={body}></Article>
+    contextControl=<><li><a href={'/update/'+id} onClick={(event)=>{
+      event.preventDefault()
+      setMode('UPDATE')
+    }}>UPDATE</a></li>
+    <li><input type="button" value="Delete" onClick={()=>{
+      const newTopics=[]
+      for(let i=0;i<topics.length;i++){
+        if(topics[i].id !== id){
+          newTopics.push(topics[i])
+        }
+      }
+      setTopics(newTopics)
+      setMode('WELCOME')
+    }}></input></li>
+    </> 
   }else if(mode ==='CREATE'){
     content=<Create onCreate={(title,body)=>{
       const newTopic = {id:nextId,title:title,body:body}
@@ -102,6 +146,31 @@ function App() {
       setNextId(nextId+1)
       setMode('READ')
     }}></Create>
+
+
+  }else if(mode ==='UPDATE'){
+    let title,body=null;
+    for(let i=0;i<topics.length;i++){
+      
+      if(topics[i].id === id){
+        title=topics[i].title;
+        body=topics[i].body;
+      }
+    }
+    content=<Update title={title} body={body} onUpdate={(title,body)=>{
+      const updatedTopic = {id:id , title:title, body:body}
+      const newTopics=[...topics]
+      for(let i=0;i<newTopics.length;i++){
+        if(newTopics[i].id === id){
+          newTopics[i]=updatedTopic
+          break;
+        }
+      }
+      // newTopics[id]=updatedTopic
+
+      setTopics(newTopics)
+      setMode('READ')
+    }}></Update>
   }
 
 
@@ -118,10 +187,14 @@ function App() {
         setId(_id)
       }}></Nav>
       {content}
-      <a href='/create' onClick={(event)=>{
-        event.preventDefault()
-        setMode('CREATE')
-      }}>CREATE</a>
+      <ul>
+        <li><a href='/create' onClick={(event)=>{
+          event.preventDefault()
+          setMode('CREATE')
+        }}>CREATE</a></li>
+       {contextControl}
+      </ul>
+      
     </div>
   );
 }
