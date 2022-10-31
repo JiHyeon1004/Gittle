bfc6222e6d643c00cc2adb146c7234f8ef11b874
junghyun1009 <97649325+junghyun1009@users.noreply.github.com>
2022-10-31 15:21:54 +0900

    [#S07P31A503-189][FE] Feat : get the last commit codes of the branch

--- gittle/src/components/addPage/GitDiff.jsx
+++ gittle/src/components/addPage/GitDiff.jsx
@@ -10,6 +10,8 @@ export default function GitDiff() {
   const [message, setMessage] = useState("");
   // 마지막 커밋 로그
   const [commit, setCommit] = useState("");
+  // 마지막 커밋 변경 파일 목록
+  const [files, setFiles] = useState([]);
   useEffect(() => {
     // 해당 branch 정보 가져오기
     // auth, owner, repo, branch 변수에 저장해서 사용해야 함
@@ -36,11 +38,42 @@ export default function GitDiff() {
     }
     getBranch();
   }, []);
+
+  useEffect(() => {
+    // 해당 branch 마지막 커밋 정보 가져오기
+    // auth, owner, repo 변수에 저장해서 사용해야 함
+    async function getCommit() {
+      const octokit = new Octokit({
+        auth: "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",
+      });
+
+      const commitInfo = await octokit.request(
+        "GET /repos/{owner}/{repo}/commits/{ref}",
+        {
+          owner: "junghyun1009",
+          repo: "TIL",
+          ref: commit,
+        }
+      );
+
+      console.log("11", commitInfo);
+      setFiles(commitInfo.data.files);
+    }
+    getCommit();
+  }, [commit]);
   return (
     <>
       <p>최종 수정 시간 : {date}</p>
       <p>변경한 사람 : {user}</p>
       <p>커밋 메세지 : {message}</p>
+      <div>
+        {files.map((file, index) => (
+          <div key={index}>
+            <p>{file.filename}</p>
+            <p>{file.patch}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
