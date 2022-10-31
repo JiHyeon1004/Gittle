86069f958327918255d2aaa19b5368d5f6889503
junghyun1009 <97649325+junghyun1009@users.noreply.github.com>
2022-10-31 15:01:43 +0900

    [#S07P31A503-188][FE] Feat : get the last commit message of the branch

--- gittle/src/components/addPage/GitDiff.jsx
+++ gittle/src/components/addPage/GitDiff.jsx
@@ -2,8 +2,14 @@ import React, { useEffect, useState } from "react";
 import { Octokit } from "octokit";
 
 export default function GitDiff() {
+  // 마지막으로 커밋한 날짜
   const [date, setDate] = useState("");
+  // 마지막으로 커밋한 사람
   const [user, setUser] = useState("");
+  // 마지막 커밋 메세지
+  const [message, setMessage] = useState("");
+  // 마지막 커밋 로그
+  const [commit, setCommit] = useState("");
   useEffect(() => {
     // 해당 branch 정보 가져오기
     // auth, owner, repo, branch 변수에 저장해서 사용해야 함
@@ -25,6 +31,8 @@ export default function GitDiff() {
       console.log(branch.data.commit.commit.author);
       setDate(branch.data.commit.commit.author.date);
       setUser(branch.data.commit.commit.author.name);
+      setMessage(branch.data.commit.commit.message);
+      setCommit(branch.data.commit.sha);
     }
     getBranch();
   }, []);
@@ -32,6 +40,7 @@ export default function GitDiff() {
     <>
       <p>최종 수정 시간 : {date}</p>
       <p>변경한 사람 : {user}</p>
+      <p>커밋 메세지 : {message}</p>
     </>
   );
 }
