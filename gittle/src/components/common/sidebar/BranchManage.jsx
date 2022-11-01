ad062280136e6727e1875ba5efa2610dfecd0a43
uussong <yoosong0713@gmail.com>
2022-11-02 00:59:49 +0900

    [#S07P31A503-173][FE] Feat : devide add branch button & modal

--- gittle/src/components/common/sidebar/BranchManage.jsx
+++ gittle/src/components/common/sidebar/BranchManage.jsx
@@ -1,51 +1,13 @@
-import React, { useState } from "react";
-import Button from "../Button";
-import Modal from "../Modal";
+import React from "react";
+import AddBranch from "./AddBranch";
+import DeleteBranch from "./DeleteBranch";
 
 function BranchManage() {
-  const [modalOpen, setModalOpen] = useState(false);
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-  const closeModal = () => {
-    setModalOpen(false);
-  };
   return (
-    <div>
-      <Button
-        action={openModal}
-        content={"브랜치 추가"}
-        style={{ backgroundColor: "#6BCC78" }}
-      />
-      <Button
-        action={openModal}
-        content={"브랜치 삭제"}
-        style={{ backgroundColor: "#6BCC78" }}
-      />
-
-      <Modal
-        open={modalOpen}
-        content={
-          targetModal === "add" ? (
-            <>
-              <label>이름</label> <input type="text" />
-            </>
-          ) : (
-            <>삭제</>
-          )
-        }
-      >
-        <div>
-          <Button content={"hi"} style={{ backgroundColor: "#6BCC78" }} />
-          <Button
-            action={closeModal}
-            content={"bye"}
-            style={{ border: "1px solid #7B7B7B" }}
-          />
-        </div>
-      </Modal>
-    </div>
+    <>
+      <AddBranch />
+      <DeleteBranch />
+    </>
   );
 }
 
