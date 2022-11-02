ec4204c158ca4cdf13dc87e4929d0338695bfc42
JiHyeon Jeong <gus4805@naver.com>
2022-11-02 16:54:20 +0900

    [#S07P31A503-177][BE] Feat : Github Oauth

--- gittle/src/App.js
+++ gittle/src/App.js
@@ -1,16 +1,29 @@
 import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Main from "./Pages/MainPage";
 import Add from "./Pages/AddPage";
+import Oauth from "./Pages/OauthPage";
+import React from "react";
+
 
 function App() {
   const location = useLocation();
+
+
+
+
+
   return (
     <div>
-      <Link to="/main">main</Link> | <Link to="/add">add</Link>
+      <Link to="/main">main</Link> | <Link to="/add">add</Link> | <Link to="/oauth">oauth</Link>
       <Routes location={location}>
         <Route path="/main" element={<Main />} />
         <Route path="/add" element={<Add />} />
+        <Route path="/oauth" element={<Oauth />} />
       </Routes>
+
+
+
+
     </div>
   );
 }
