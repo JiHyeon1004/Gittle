34236f268437776ff895a107245983c59b77edd9
junghyun1009 <97649325+junghyun1009@users.noreply.github.com>
2022-11-02 15:28:25 +0900

    [#S07P31A503-195][FE] Design : change the font

--- gittle/src/App.js
+++ gittle/src/App.js
@@ -2,11 +2,12 @@ import { Link, Route, Routes, useLocation } from "react-router-dom";
 import Main from "./pages/MainPage";
 import Add from "./pages/AddPage";
 import Log from "./pages/LogPage";
+import "./App.css";
 
 function App() {
   const location = useLocation();
   return (
-    <div>
+    <div className="App">
       <Link to="/main">main</Link> | <Link to="/add">add</Link> |{" "}
       <Link to="/log">log</Link>
       <Routes location={location}>
