43c581b38bb1533a0e9561a7e698f225d9444b76
ssong <77879633+uussong@users.noreply.github.com>
2022-11-03 09:40:09 +0900

    [#S07P31A503-176][FE] Design : 기본 구조 디자인 수정

---
+++ gittle/src/components/common/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <h2>Gittle</h2>
+      <div>
+        <Link to="/main">main</Link> | <Link to="/add">add</Link> |{" "}
+        <Link to="/oauth">oauth</Link> | <Link to="/log">log</Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
