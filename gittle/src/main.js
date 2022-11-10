ba15e9d220f0b5f6b4ef15043d9fe29d64e4de92
sun <zpfh1638@naver.com>
2022-11-10 13:32:04 +0900

    test
    Former-commit-id: 4f15423c9a6a837f7645fd5992502444885eb9fb

--- gittle/src/main.js
+++ gittle/src/main.js
@@ -330,9 +330,10 @@ ipcMain.on("lastCommitDescription", (event, payload,branch) => {
 ipcMain.on("git-Push",(event,payload)=>{
   console.log("repo입니다 : ",payload.repoRoot)
   console.log("브랜치입니다 : ",payload.branch)
-  runCommand(`
+  let ment = runCommand(`
     cd "${payload.repoRoot}" && git push origin ${payload.branch}
   `)
+  console.log("ment : ",ment)
   console.log("완료되었습니다")
 })
 ipcMain.on("gitbash",(event, currentRepo) =>{
