66f8dc58e711b1a916169851830d069c2a94ab33
sun <zpfh1638@naver.com>
2022-11-15 17:18:40 +0900

    test
    Former-commit-id: 1b14dc7e731678dab1b8ff325b4a5b8a1d7e64a5

--- gittle/src/Components/common/underbar/Command.jsx
+++ gittle/src/Components/common/underbar/Command.jsx
@@ -1,20 +1,39 @@
-import React from "react";
+import React,{useState} from "react";
 import styles from "./Command.module.css"
 
 function Command(props){
 
     let cmd=props.cmd
+    let arr =cmd.split('\n')
+    const [isActive,SetIsActive] = useState(false)
 
     return(
         <>
-            <div className={styles.cmdBox}>
-            {cmd.split('\n').map((item, idx)=>(
-                <div key={idx} className={styles.cmd}>
-                    {item}
+            <div className={!isActive ? styles.cmdBox : styles.bigCmdBox}>
+                <div className={styles.button} onClick={()=>{
+                    
+                    SetIsActive(!isActive)
+                }}>
+                    {!isActive && '^'}
+                    {isActive && 'V'}
                 </div>
-            ))}
-        </div>
-        </>
+                <div className={styles.wordBox}>
+                    {!isActive && 
+                        <div className={styles.cmd}>
+                        {arr[arr.length-1]}
+                    </div> }
+                    
+                    {isActive && 
+                        arr.map((item, idx)=>(
+                            <div key={idx} className={styles.cmd}>
+                                {item}
+                            </div>
+                        ))
+                    }
+                </div>
+                
+            </div>
+            </>
         
     )
 }
