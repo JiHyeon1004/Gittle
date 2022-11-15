508a377a70f0eb18595d0252a75c5879eca9a92c
sun <zpfh1638@naver.com>
2022-11-15 15:36:33 +0900

    test
    Former-commit-id: 0ce6e8aa89261d8289b687f4d0c762c14255490f

--- gittle/src/Components/common/underbar/Command.jsx
+++ gittle/src/Components/common/underbar/Command.jsx
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 import styles from "./Command.module.css"
 
 function Command(props){
-    console.log("결과 : ",props.cmd)
+    console.log("결과 : ",JSON.parse(props.cmd))
     console.log("뿌에에에엥 : ",props.cmd[0])
     let arr = JSON.parse(props.cmd);
     for(let i=0;i<arr.length;i++){
