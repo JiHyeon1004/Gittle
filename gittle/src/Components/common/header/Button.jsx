7ebd7390689293eeb453856b579ddaf779ae677a
sun <zpfh1638@naver.com>
2022-11-17 09:36:19 +0900

    [#S07P31A503-338][FE] Feat : 페이지 이동 버튼 틀 완성
    Former-commit-id: 8e7d566f833020ecbbf8935c1dccae124563cf7c

--- gittle/src/Components/common/header/Button.jsx
+++ gittle/src/Components/common/header/Button.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import {Link} from "react-router-dom"
+import { useNavigate } from "react-router";
 import styles from "./Button.module.css"
 
 
 function Buttons(props){
-
+    const navigate = useNavigate()
     const root ="/"+props.page;
     
     return(
         <div 
             className={props.selPage === props.page ? styles.selectedPage : styles.unSelectedPage}
-            onClick={()=>{props.whenClick(props.page)}}>
-            <Link to={root} className={styles.text}>{props.page}</Link>
+            onClick={()=>{
+                props.whenClick(props.page)
+                navigate(root)
+            }}>
+            <div className={styles.text}>{props.page}</div>
         </div>
     )
 
