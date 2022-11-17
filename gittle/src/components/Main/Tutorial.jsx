1acd89e3fca7f7de6f8c236c91556601c39bd7d2
ssong <77879633+uussong@users.noreply.github.com>
2022-11-17 14:31:51 +0900

    [#S07P31A503-311][FE] Design : 메인 페이지 구조 수정
    Former-commit-id: 1b80d83da0fa7ecb733ed6bcc9abbd9be20bf894

--- gittle/src/components/Main/Tutorial.jsx
+++ gittle/src/components/Main/Tutorial.jsx
@@ -2,13 +2,7 @@ import React from "react";
 import styles from "./Tutorial.module.css";
 
 function tutorial() {
-  return (
-    <div className={styles.tutorial}>
-      시작이 어렵다면?
-      <br />
-      (튜토리얼)
-    </div>
-  );
+  return <div className={styles.tutorial}>시작이 어렵다면?</div>;
 }
 
 export default tutorial;
